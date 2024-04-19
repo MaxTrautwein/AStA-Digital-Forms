2ec335728434f14274743db6df4145144dd1527f
Max Trautwein <trautwein.max@gmail.com>
2024-04-19 11:51:08 +0200

    #24 added secrets generation

--- init_secrets.sh
+++ init_secrets.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# Generates initial secrets for docker
+
+mkdir ./secrets
+openssl rand -base64 20 > ./secrets/mongo_user
+openssl rand -base64 40 > ./secrets/mongo_pw
+openssl rand -base64 40 > ./secrets/KC_DB_PASSWORD
+openssl rand -base64 40 > ./secrets/KEYCLOAK_ADMIN_PASSWORD
+openssl rand -base64 20 > ./secrets/KEYCLOAK_ADMIN
+
+#For testing
+echo "localhost" > ./secrets/Backend_URL
+echo "localhost" > ./secrets/KC_HOSTNAME
