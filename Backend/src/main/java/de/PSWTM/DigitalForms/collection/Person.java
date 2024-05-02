ccf458a0f8ade5e2c86d59811eb8d691e836cc2f
Ebenezer2077 <tobigcb547@outlook.de>
2024-05-02 14:56:16 +0200

    added missing mongodb dependency and others. Started with making test classes (Person.java) to store in DB

---
+++ Backend/src/main/java/de/PSWTM/DigitalForms/collection/Person.java
@@ -0,0 +1,20 @@
+package de.PSWTM.DigitalForms.collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document(collation = "person")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Person {
+    @Id
+    private String PersonId;
+    private String FirstName;
+    private String SecondName;
+}
