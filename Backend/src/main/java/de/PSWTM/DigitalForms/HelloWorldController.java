91a544e43210f201365ed803a42a662519ced313
Max Trautwein <trautwein.max@gmail.com>
2024-05-04 11:17:17 +0200

    a working sample that adds a persons

--- Backend/src/main/java/de/PSWTM/DigitalForms/HelloWorldController.java
+++ Backend/src/main/java/de/PSWTM/DigitalForms/HelloWorldController.java
@@ -1,6 +1,7 @@
 package de.PSWTM.DigitalForms;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,18 @@ public class HelloWorldController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-
+    @Autowired
+    private PersonRepository repository;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/test")
+    public String greeting()  {
+        repository.save(new Person("bla"));
+        repository.save(new Person("blub"));
+        return "test";
     }
 }
