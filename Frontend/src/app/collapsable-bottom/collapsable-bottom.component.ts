cde8f8748ed23cf4ca33da6fe4220a245ae18a06
Ebenezer2077 <tobigcb547@outlook.de>
2024-05-09 17:41:27 +0200

    almost finished landing page without any functionality. Bootrstrap sources mentioned in the html comment. Arbeitszeit: 1h

---
+++ Frontend/src/app/collapsable-bottom/collapsable-bottom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collapsable-bottom',
+  standalone: true,
+  imports: [],
+  templateUrl: './collapsable-bottom.component.html',
+  styleUrl: './collapsable-bottom.component.css'
+})
+export class CollapsableBottomComponent {
+
+}
