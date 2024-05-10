aa5e866c4529e454e9efbb754178d61dfee74d17
Ebenezer2077 <tobigcb547@outlook.de>
2024-05-10 15:06:35 +0200

    Functionality for collapsable is finaly working. Arbeitszeit: 1h 30 min

--- Frontend/src/app/collapsable-bottom/collapsable-bottom.component.ts
+++ Frontend/src/app/collapsable-bottom/collapsable-bottom.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collapsable-bottom',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './collapsable-bottom.component.html',
   styleUrl: './collapsable-bottom.component.css'
 })
