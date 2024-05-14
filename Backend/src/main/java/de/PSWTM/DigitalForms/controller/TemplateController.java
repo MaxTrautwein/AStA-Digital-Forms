adaeafa865a11dbc898cc1aa85c4df1ac5fb1e9c
Max Trautwein <trautwein.max@gmail.com>
2024-05-14 14:32:49 +0200

    #50 updated spec for better naming

--- Backend/src/main/java/de/PSWTM/DigitalForms/controller/TemplateController.java
+++ Backend/src/main/java/de/PSWTM/DigitalForms/controller/TemplateController.java
@@ -1,7 +1,7 @@
 package de.PSWTM.DigitalForms.controller;
 
-import de.PSWTM.DigitalForms.api.TemplateApi;
-import de.PSWTM.DigitalForms.api.TemplateApiDelegate;
+
+import de.PSWTM.DigitalForms.api.TemplatesApiDelegate;
 import de.PSWTM.DigitalForms.model.Form;
 import de.PSWTM.DigitalForms.repository.FormRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +14,19 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class TemplateController implements TemplateApiDelegate {
+public class TemplateController implements TemplatesApiDelegate {
     @Autowired
     private FormRepository repository;
 
-
     @Override
-    public ResponseEntity<List<Form>> templateListGet() {
+    public ResponseEntity<List<Form>> templatesGet() {
         return ResponseEntity.ok(repository.findAllTemplates_IdNameDescription());
     }
 
     @Override
-    public ResponseEntity<Form> templateTemplateIdGet(String templateId) {
+    public ResponseEntity<Form> templatesTemplateIdGet(String templateId) {
         Form res = repository.findById(templateId).orElse(null);
 
         if (res == null) return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(res);
-    }
+        return ResponseEntity.ok(res);    }
 }
