6c5cb285307a39d59ede6d0efd8d8f43688c3006
Ebenezer2077 <tobigcb547@outlook.de>
2024-05-17 14:46:29 +0200

    YES! IT WORKS! Arbeitszeit: 1h15min (now its just design and maybe small quality of life bugfixes)

--- Frontend/src/app/main-buttons/main-buttons.component.ts
+++ Frontend/src/app/main-buttons/main-buttons.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
-import { DefaultService} from '../api-client';
+import { Component, OnInit } from '@angular/core';
+import { DefaultService, Form} from '../api-client';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-main-buttons',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './main-buttons.component.html',
   styleUrl: './main-buttons.component.css'
 })
-export class MainButtonsComponent{
+export class MainButtonsComponent implements OnInit{
   
   
-  constructor(private defaultservice: DefaultService) {};
-  
+  constructor(private defaultservice: DefaultService, private oauthService: OAuthService) {
+  };
+  templates: any = [];
+
   async getTemplates() {
-    this.defaultservice.templatesGet().subscribe(Response => {
+    this.defaultservice.templatesGet().subscribe((Response) => {
       alert('It worked');
+      this.templates = Response;
     });
+  }
+
+  ngOnInit(): void {
+    this.defaultservice.configuration.accessToken = this.oauthService.getAccessToken();
+    this.getTemplates();
   }
 }
