094e2013c0df7d598235c7349b92c0f3ff4c8459
Max Trautwein <trautwein.max@gmail.com>
2024-05-19 13:57:55 +0200

    added routing to allow diffrent views

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import {RouterModule, } from '@angular/router';
 import {NgModule} from '@angular/core';
-export const routes: Routes = [];
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {FormContainerComponent} from "./form-container/form-container.component";
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'Form', component: FormContainerComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
