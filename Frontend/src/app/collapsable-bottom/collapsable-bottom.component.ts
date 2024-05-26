04ccbe781537ff2626cd0f65dfb88e5ba04ef0d1
Max Trautwein <trautwein.max@gmail.com>
2024-05-26 12:17:10 +0200

    #76 added Loading Support

--- Frontend/src/app/collapsable-bottom/collapsable-bottom.component.ts
+++ Frontend/src/app/collapsable-bottom/collapsable-bottom.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {MarkedItemComponent} from "./marked-item/marked-item.component";
+import {Form, FormsService} from "../api-client";
+import {Observable} from "rxjs";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {PrepareAPIService} from "../prepare-api.service";
 
 @Component({
   selector: 'app-collapsable-bottom',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MarkedItemComponent, AsyncPipe, NgForOf],
   templateUrl: './collapsable-bottom.component.html',
   styleUrl: './collapsable-bottom.component.css'
 })
 export class CollapsableBottomComponent {
+
+  protected forms: Observable<Form[]>
+
+  constructor(private api: FormsService, private prep: PrepareAPIService) {
+    prep.prepare();
+    this.forms = api.formsGet()
+  }
 
 }
