439a6b6786622e121d8aa4c17ec589d940b82835
Ebenezer2077 <tobigcb547@outlook.de>
2024-05-29 22:33:49 +0200

    made requested changes and got rid of unused imports

--- Frontend/src/app/main-buttons/main-buttons.component.ts
+++ Frontend/src/app/main-buttons/main-buttons.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { DefaultService } from '../api-client';
-import { OAuthService } from 'angular-oauth2-oidc';
 import { NgFor, NgIf } from '@angular/common';
 import {RouterLink} from "@angular/router";
 import {TemplateService} from "../template.service";
-import { Form } from '../api-client';
 
 
 @Component({
