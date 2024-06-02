4774d8e182007f0e99400668c3e443f06c677c12
HydroZocker <149052852+HydroZocker@users.noreply.github.com>
2024-06-03 01:23:37 +0200

    IBAN Validierung (unfertig)

--- Frontend/src/app/form-container/form-content/iban/iban.component.ts
+++ Frontend/src/app/form-container/form-content/iban/iban.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, AfterViewInit } from '@angular/core';
+import { ValidatorService} from 'angular-iban';
 
 @Component({
   selector: 'app-iban',
   standalone: true,
-  imports: [],
   templateUrl: './iban.component.html',
-  styleUrl: './iban.component.css'
+  styleUrls: ['./iban.component.css'],
+  providers: [ValidatorService]
 })
-export class IbanComponent {
+export class IbanComponent implements AfterViewInit {
+  @Input() description: string | undefined = "";
+  @Input() help: string | undefined = "";
+  @Input() value: string | undefined = "";
+  @Output() valueChanged = new EventEmitter<string>();
 
+  @ViewChild('input') input: any;
+  isValidIban: boolean = true; // Flag für die IBAN-Validierung
+
+  constructor(private validatorService: ValidatorService) {}
+
+  ngAfterViewInit() {
+    if (this.value === undefined) {
+      this.value = "";
+    }
+    this.input.nativeElement.value = this.value;
+  }
+
+  onValueChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;
+    this.isValidIban = this.validatorService.validateIban(this.value); // IBAN validieren
+    this.valueChanged.emit(this.value);
+  }
 }
