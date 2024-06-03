a2fff60d786ec60b8e63e3c02c184f02d2fdf122
HydroZocker <149052852+HydroZocker@users.noreply.github.com>
2024-06-03 18:51:06 +0200

    Datumeingabe

--- Frontend/src/app/form-container/form-content/date/date.component.ts
+++ Frontend/src/app/form-container/form-content/date/date.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-date',
   standalone: true,
-  imports: [],
   templateUrl: './date.component.html',
-  styleUrl: './date.component.css'
+  styleUrls: ['./date.component.css']
 })
-export class DateComponent {
+export class DateComponent implements AfterViewInit {
+  @Input() description: string | undefined = "";
+  @Input() help: string | undefined = "";
+  @Input() value: string | undefined = "";
 
+  @Output() valueChanged = new EventEmitter<string>();
+
+  @ViewChild('input') input: any;
+  isValidDate: boolean = true;
+
+  ngAfterViewInit() {
+    if (this.value === undefined) {
+      this.value = "";
+    }
+    this.input.nativeElement.value = this.value;
+  }
+
+  onValueChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;
+    this.isValidDate = this.validateDate(this.value);
+    this.valueChanged.emit(this.value);
+  }
+
+  validateDate(dateString: string): boolean {
+    const date = new Date(dateString);
+    const minDate = new Date('1900-01-01');
+    const maxDate = new Date();
+    if (isNaN(date.getTime())) {
+      return false;
+    }
+    return date >= minDate && date <= maxDate;
+  }
 }
