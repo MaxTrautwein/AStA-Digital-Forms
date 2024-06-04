d2d22222ba8c395c29d71e1ba073dae30287f38e
HydroZocker <149052852+HydroZocker@users.noreply.github.com>
2024-06-04 04:28:53 +0200

    Eingabe eines Betrags

--- Frontend/src/app/form-container/form-content/money/money.component.ts
+++ Frontend/src/app/form-container/form-content/money/money.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {FormSection} from "../../../api-client";
 
 @Component({
   selector: 'app-money',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './money.component.css'
 })
 export class MoneyComponent {
+@Input() description: string | undefined = "Betrag";
+@Input() help: string | undefined = "";
+@Input() value: string | undefined = "";
 
+@Output() valueChanged = new EventEmitter<string>();
+
+@ViewChild('input') input: any;
+
+  ngAfterViewInit(){
+    if (this.value === undefined){
+      this.value = ""
+    }
+    this.input.nativeElement.value = this.value;
+  }
+
+  onValueChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.valueChanged.emit(inputElement.value);
+  }
 }
