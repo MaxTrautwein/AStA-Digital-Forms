758835d4ef04dcd82e4c92027b8aa36934e30cda
HydroZocker <149052852+HydroZocker@users.noreply.github.com>
2024-06-04 03:51:27 +0200

    Text Multi Line, IBAN vorerst deaktiviert

--- Frontend/src/app/form-container/form-content/iban/iban.component.ts
+++ Frontend/src/app/form-container/form-content/iban/iban.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, Output, ViewChild, AfterViewInit } from '@angular/core';
-import { ValidatorService} from 'angular-iban';
+//import { ValidatorService} from 'angular-iban';
 
 @Component({
   selector: 'app-iban',
   standalone: true,
   templateUrl: './iban.component.html',
   styleUrls: ['./iban.component.css'],
-  providers: [ValidatorService]
+  providers: []
 })
 export class IbanComponent implements AfterViewInit {
   @Input() description: string | undefined = "";
@@ -17,7 +17,7 @@ export class IbanComponent implements AfterViewInit {
   @ViewChild('input') input: any;
   isValidIban: boolean = true; // Flag für die IBAN-Validierung
 
-  constructor(private validatorService: ValidatorService) {}
+  //constructor(private validatorService: ValidatorService) {}
 
   ngAfterViewInit() {
     if (this.value === undefined) {
@@ -29,7 +29,7 @@ export class IbanComponent implements AfterViewInit {
   onValueChange(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     this.value = inputElement.value;
-    this.isValidIban = this.validatorService.validateIban(this.value); // IBAN validieren
+   // this.isValidIban = this.validatorService.validateIban(this.value); // IBAN validieren
     this.valueChanged.emit(this.value);
   }
 }
--- Frontend/src/app/form-container/form-content/text-multi-line/text-multi-line.component.ts
+++ Frontend/src/app/form-container/form-content/text-multi-line/text-multi-line.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-text-multi-line',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './text-multi-line.component.html',
   styleUrl: './text-multi-line.component.css'
 })
-export class TextMultiLineComponent {
-
-}
+export class TextMultiLineComponent  {
+  @Input() description: string | undefined = "MultiTextFeld";
+  @Input() help: string | undefined = "";
+  @Input() value: string | undefined = "";
+  
+  @Output() valueChanged = new EventEmitter<string>();
+  
+  @ViewChild('input') input: any;
+  
+    ngAfterViewInit(){
+      if (this.value === undefined){
+        this.value = ""
+      }
+      this.input.nativeElement.value = this.value;
+    }
+  
+    onValueChange(event: Event) {
+      const inputElement = event.target as HTMLInputElement;
+      this.valueChanged.emit(inputElement.value);
+    }
+  }
+  
