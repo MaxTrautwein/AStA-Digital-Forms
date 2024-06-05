1f5b4000b2613b4e12dd41ff83b581dd2ef53b1b
Ebenezer2077 <tobigcb547@outlook.de>
2024-06-05 14:52:49 +0200

    accidentaly deleted stuff from the openapi spec. Arbeitszeit: 30 min

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { FormContainerComponent } from "./form-container/form-container.component";
 import { LoginComponent } from './login/login.component';
+import { UserDataComponent } from './user-data/user-data.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent},
-  { path: 'Form/:id', component: FormContainerComponent }
+  { path: 'Form/:id', component: FormContainerComponent },
+  { path: 'userdata', component: UserDataComponent}
 ];
 
 @NgModule({
--- Frontend/src/app/form-container/form-container.component.ts
+++ Frontend/src/app/form-container/form-container.component.ts
@@ -108,7 +108,6 @@ export class FormContainerComponent {
     this.api.formsFormIDDownloadGet(id).subscribe((response) => {
       const downloadLink = document.createElement('a');
       downloadLink.href = URL.createObjectURL(response);
-
       const fileName = "test"
       downloadLink.download = fileName;
       downloadLink.click();
