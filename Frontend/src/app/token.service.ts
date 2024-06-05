539640c1f137b553ac982a4df9172aa703881f61
Max Trautwein <trautwein.max@gmail.com>
2024-06-05 18:58:15 +0200

    #108 added Token service to check for active token and then Notify and set it for all API calls

---
+++ Frontend/src/app/token.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {OAuthService} from "angular-oauth2-oidc";
+import {Observable, Subscriber} from "rxjs";
+import {DefaultService, FavouritesService, FormsService, GroupsService} from "./api-client";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  private emitter: Subscriber<Boolean> = new Subscriber<Boolean>();
+
+  // Notify That a Token is ready
+  public tokenReady:  Observable<Boolean>;
+
+  constructor(private oauthService: OAuthService, private defaultService: DefaultService, private formService: FormsService,
+              private groupService: GroupsService, private favoritesService: FavouritesService) {
+    this.tokenReady = new Observable(e => this.emitter = e);
+    //this.emitter.next(false);
+
+    this.oauthService.events.subscribe(e => {
+
+      if (e.type === "token_received" || (e.type === "discovery_document_loaded" && this.oauthService.hasValidAccessToken())){
+
+        this.defaultService.configuration.credentials["BearerAuth"] = this.oauthService.getAccessToken();
+        this.formService.configuration.credentials["BearerAuth"] = this.oauthService.getAccessToken();
+        this.groupService.configuration.credentials["BearerAuth"] = this.oauthService.getAccessToken();
+        this.favoritesService.configuration.credentials["BearerAuth"] = this.oauthService.getAccessToken();
+
+        if(!this.emitter.closed){
+          this.emitter.next(true);
+          this.emitter.complete();
+        }
+
+      }
+
+
+    })
+
+
+  }
+
+
+
+}
