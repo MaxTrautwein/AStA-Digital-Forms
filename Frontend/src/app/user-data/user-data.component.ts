d3d7b6ec04aa178c36ae0d905b0c961d95f4fd53
Ebenezer2077 <tobigcb547@outlook.de>
2024-06-05 17:24:07 +0200

    frontendpart for saving is done. Arbeitszeit: 1h 10min

--- Frontend/src/app/user-data/user-data.component.ts
+++ Frontend/src/app/user-data/user-data.component.ts
@@ -1,6 +1,9 @@
 import { Component} from '@angular/core';
 import { UserDataService } from '../api-client';
 import { FormsModule } from '@angular/forms';
+import { DefaultService } from '../api-client';
+import { OnInit } from '@angular/core';
+import { UserData } from '../api-client';
 
 
 @Component({
@@ -15,14 +18,40 @@ import { FormsModule } from '@angular/forms';
 
 
 
-export class UserDataComponent {
-  public inputWert: string | undefined;
+export class UserDataComponent implements OnInit{
+  
+  public name: string | undefined;
+  public vorname: string | undefined;
+  public adresse: string | undefined;
+  public email: string | undefined;
+  public iban: string | undefined;
+  public kreditinstitut: string | undefined;
 
-  constructor(private userservice: UserDataService) {}
+  private ud: UserData = {};
+  
 
-  onSubmit() {
-    console.log(this.inputWert);
+  constructor(private userservice: UserDataService, private def: DefaultService) {}
 
+  sync() {
+    this.ud.name = this.name;
+    this.ud.firstName = this.vorname;
+    this.ud.adress = this.adresse;
+    this.ud.email = this.email;
+    this.ud.IBAN = this.iban;
+    this.ud.CreditInstitute = this.kreditinstitut;
+  }
+
+  ngOnInit(): void {
+    //this.ud = this.userservice.userDataGet();
+      
     
   }
+
+  save() {
+    this.sync();
+    this.userservice.userDataPost(this.ud);
+
+  }
+
+  
 }
