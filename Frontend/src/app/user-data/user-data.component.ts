08be8394c02b9546243db581752415c9550d684e
Ebenezer2077 <tobigcb547@outlook.de>
2024-06-05 16:13:54 +0200

    finaly, reading data from input works!

--- Frontend/src/app/user-data/user-data.component.ts
+++ Frontend/src/app/user-data/user-data.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { UserDataService } from '../api-client';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-data',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-data.component.html',
   styleUrl: './user-data.component.css'
 })
-export class UserDataComponent {
 
+
+
+
+
+export class UserDataComponent {
+  public inputWert: string | undefined;
+
+  constructor(private userservice: UserDataService) {}
+
+  onSubmit() {
+    console.log(this.inputWert);
+
+  }
 }
