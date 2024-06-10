5fb8a4f6b912aadbfc9352f855285a1bf48bd17a
Max Trautwein <trautwein.max@gmail.com>
2024-06-10 22:08:53 +0200

    implemented endpoint in the frontend

--- Frontend/src/app/form-container/overview/overview.component.ts
+++ Frontend/src/app/form-container/overview/overview.component.ts
@@ -1,20 +1,35 @@
 import {Component, Input} from '@angular/core';
-import {Attachment, Form, FormsService} from "../../api-client";
+import {Attachment, Favourite, Form, FormsService} from "../../api-client";
 import {AttachmentComponent} from "./attachment/attachment.component";
+import {TokenService} from "../../token.service";
+import {Observable, Subject, Subscriber} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-overview',
   standalone: true,
   imports: [
-    AttachmentComponent
+    AttachmentComponent,
+    AsyncPipe
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
-  @Input() form!: Form;
+  @Input() formId!: string;
 
-  constructor(private api: FormsService) {
+  private attachmentsEmitter: Subject<Attachment[]> = new Subject<Attachment[]>();
+  protected attachments: Observable<Attachment[]>;
+
+  constructor(private api: FormsService, private tokenService: TokenService) {
+    this.attachments = this.attachmentsEmitter.asObservable();
+  }
+
+  protected filterRequired(attach: Attachment[]): Attachment[]{
+    return attach.filter( a => a.required === Attachment.RequiredEnum.Conditional || a.required === Attachment.RequiredEnum.Always)
+  }
+  protected filterUser(attach: Attachment[]): Attachment[]{
+    return attach.filter( a => a.required === Attachment.RequiredEnum.User)
   }
 
   DownloadPDF(id: string){
@@ -25,6 +40,26 @@ export class OverviewComponent {
       downloadLink.download = fileName;
       downloadLink.click();
     })
+  }
+
+  private Initialize(){
+    this.api.formsFormIDAttchmentsGet(this.formId).subscribe( a => {
+      this.attachmentsEmitter.next(a);
+    })
+
+  }
+
+  ngOnInit() {
+
+    if (!this.tokenService.hasValidToken()){
+      this.tokenService.tokenReady.subscribe(s => {
+        if (s){
+          this.Initialize();
+        }
+      })
+    }else {
+      this.Initialize();
+    }
   }
 
   protected readonly Attachment = Attachment;
