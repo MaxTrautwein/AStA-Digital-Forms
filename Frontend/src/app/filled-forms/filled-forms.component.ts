28aaf31793187487ca9ee3e6ee7660c020b16a85
Max Trautwein <trautwein.max@gmail.com>
2024-06-11 11:21:05 +0200

    #105 added search function updated looks

--- Frontend/src/app/filled-forms/filled-forms.component.ts
+++ Frontend/src/app/filled-forms/filled-forms.component.ts
@@ -5,6 +5,7 @@ import {TokenService} from "../token.service";
 import {Favourite, FavouritesService, Form, FormsService} from "../api-client";
 import {TemplateService} from "../template.service";
 import {Observable, Subject, Subscriber} from "rxjs";
+import {SearchService} from "../search.service";
 
 @Component({
   selector: 'app-filled-forms',
@@ -19,13 +20,24 @@ import {Observable, Subject, Subscriber} from "rxjs";
 export class FilledFormsComponent {
 
 
-  constructor(private tokenService: TokenService, private api: FormsService) {
+  constructor(private tokenService: TokenService, private api: FormsService, protected search: SearchService) {
     this.forms = this.formsEmitter.asObservable();
   }
 
   protected forms: Observable<Form[]>;
   private formsEmitter: Subject<Form[]> = new Subject<Form[]>();
 
+
+  protected getFormShortName(form: Form){
+    let ret = "";
+    form.form?.forEach(fs => {
+      const shortName = fs.items?.find(fe => fe.id == "bez")?.value
+      if(shortName){
+        ret = shortName;
+      }
+    })
+    return ret;
+  }
 
   private Init(){
     this.api.formsGet().subscribe( forms => {
