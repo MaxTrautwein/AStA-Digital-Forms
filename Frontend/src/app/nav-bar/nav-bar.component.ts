87e07d819a2692b650a4537beb2f9cd8984ca938
Ebenezer2077 <tobigcb547@outlook.de>
2024-06-16 21:34:20 +0200

    made  the rest scallable and added little feature for account holder. Arbeitszeit: 30 min

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import {SearchService} from "../search.service";
+import { OAuthService } from 'angular-oauth2-oidc';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,8 +13,9 @@ import {SearchService} from "../search.service";
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
   protected isOpen: boolean = false;
+  protected Username: string = "Account";
 
   onClick() {
     if(this.isOpen) {
@@ -22,10 +25,21 @@ export class NavBarComponent {
     }
   }
 
-  constructor(protected search: SearchService) {
+  ngOnInit(): void {
+      this.test();
+  }
+
+  constructor(protected search: SearchService, protected oauthService: OAuthService) {
 
   }
 
+  test() {
+    let claims = this.oauthService.getIdentityClaims();
+    if(!claims) return null;
+    this.Username = claims['given_name'];
+    console.log(claims['given_name']);
+    return null;
+  }
 
 
 }
